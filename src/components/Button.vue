a58535e435f9ed02a1d5f445d8228e3f5d5a7139
sanjeevyg <yogi.sjv@gmail.com>
2022-07-23 23:42:54 -0400

    button and header added

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+    methods: {
+      onclick() {
+        console.log("click");
+      },
+    },
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+  <Button text="Add Task" color="green" />
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+  name: "HeaderA",
+  props: ["title"],
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
